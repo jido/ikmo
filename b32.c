b72ad22a946276062d796c414548130c0f55e1fa
jido <denisgithub@bredelet.com>
2019-05-30 07:59:32 +0100

    Describe alternative alphabets
     00  ( @ `
     01  ) A a
     02  * B b      -- use * if worried someone will read B as 8
     03  + C c
     04  , D d
     05  - E e
     06  . F f
     07  / G g
     08  [space] H h
     09  ! I i      -- similar looking characters are equivalent
     0a  " J j
     0b  # K k
     0c  $ L l      -- use $ if worried someone will read l as 1 or I
     0d  % M m
     0e  & N n
     0f  ' O o      -- unfortunately 0 is not equivalent to O
     10  8 P p
     11  9 Q q      -- use 9 if worried someone will read Q as O
     12  : R r
     13  ; S s      -- unfortunately 5 is not equivalent to S
     14  < T t
     15  = U u
     16  > V v
     17  ? W w
     18  0 X x
     19  1 Y y
     1a  2 Z z      -- similar looking characters are equivalent
     1b  3 [ {
     1c  4 | \
     1d  5 ] }
     1e  6 ^ ~
     1f  7 _

--- b32.c
+++ b32.c
@@ -3,11 +3,46 @@
 
 typedef unsigned long long u64;
 
+/*
+ Alternative alphabets:
+ 00  ( @ `
+ 01  ) A a
+ 02  * B b      -- use * if worried someone will read B as 8
+ 03  + C c
+ 04  , D d
+ 05  - E e
+ 06  . F f
+ 07  / G g
+ 08  [space] H h
+ 09  ! I i      -- similar looking characters are equivalent
+ 0a  " J j
+ 0b  # K k
+ 0c  $ L l      -- use $ if worried someone will read l as 1 or I
+ 0d  % M m
+ 0e  & N n
+ 0f  ' O o      -- unfortunately 0 is not equivalent to O
+ 10  8 P p
+ 11  9 Q q      -- use 9 if worried someone will read Q as O
+ 12  : R r
+ 13  ; S s      -- unfortunately 5 is not equivalent to S
+ 14  < T t
+ 15  = U u
+ 16  > V v
+ 17  ? W w
+ 18  0 X x
+ 19  1 Y y
+ 1a  2 Z z      -- similar looking characters are equivalent
+ 1b  3 [ {
+ 1c  4 | \
+ 1d  5 ] }
+ 1e  6 ^ ~
+ 1f  7 _
+*/
 const char b32table[32] = {
-  'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o',
   '@', 'a', 'b', 'c', 'd', 'e', 'f', 'g',
-  'x', 'y', 'z', '3', '4', '5', '6', '7',
-  'p', 'q', 'r', 's', 't', 'u', 'v', 'w' 
+  'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o',
+  'p', 'q', 'r', 's', 't', 'u', 'v', 'w',
+  'x', 'y', 'z', '3', '4', '5', '6', '7'
 };
 
 size_t b32encode(char *from, char *to, size_t len) {
@@ -65,11 +100,11 @@ size_t b32encode(char *from, char *to, size_t len) {
 
 size_t b32decode(char *from, char *to, size_t len) {
     size_t es = len / 13 * sizeof(u64);
-    while (len >= 13)
+    while (len >= 16)
     {
         u64 *bin = (u64 *)to;
         u64 *code0 = (u64 *)from;
-        u64 t =  *code0 & 0x4040404040404040LL;
+        u64 t =  ~*code0 & 0x4040404040404040LL;
         u64 lo = (*code0 & 0x1f1f1f1f1f1f1f1fLL) ^ (t >> 3);
         char *bytes = (char *)&lo;
         u64 dest = (u64)bytes[0]
@@ -81,7 +116,7 @@ size_t b32decode(char *from, char *to, size_t len) {
             | ((u64)bytes[6] << 30)
             | ((u64)bytes[7] << 35);
         u64 code1 = (u64)from[8] | ((u64)from[9] << 8) | ((u64)from[10] << 16) | ((u64)from[11] << 24) | ((u64)from[12] << 32);
-        t =  code1 & 0x4040404040404040LL;
+        t =  ~code1 & 0x4040404040404040LL;
         lo = (code1 & 0x1f1f1f1f1f1f1f1fLL) ^ (t >> 3);
         dest |= ((u64)(bytes[0]) << 40) | ((u64)bytes[1] << 45) | ((u64)bytes[2] << 50) | ((u64)bytes[3] << 55) | ((u64)bytes[4] << 60);
         *bin = dest;
